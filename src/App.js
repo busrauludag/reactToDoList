a3e5087a71d636da10b0d97afa20446fc7a607bf
Busra Uludag <busrauludag@Busra-MacBook-Air.local>
2020-07-19 02:26:25 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import '../node_modules/font-awesome/scss/font-awesome.scss';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 import SignIn from './components/pages/SignIn';
-import HomeList from './components/pages/HomeList';
+import List from './components/pages/List';
 import Detail from './components/pages/Detail';
 import NotFound from './components/pages/NotFound';
 
@@ -33,7 +33,7 @@ function App() {
             }}
           />
           <Route exact path="/login" component={SignIn} />
-          <Route exact path="/todolist" component={HomeList} />
+          <Route exact path="/todolist" component={List} />
           <Route exact path="/todolist/:id" component={Detail} />
           <Route component={NotFound}/>
         </Switch>
