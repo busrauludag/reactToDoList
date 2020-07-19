73bf666318e7ca3e2f6967f668ded537e83df9f6
Busra Uludag <busrauludag@Busra-MacBook-Air.local>
2020-07-19 17:25:59 +0300

    url fix

--- src/urls.js
+++ src/urls.js
@@ -1,2 +1,1 @@
-// export const todoUrl = 'http://localhost:3003/todos/';
-export const todoUrl = 'https://react-to-do-list.busrauludag.vercel.app/todos/';
+export const todoUrl = 'http://localhost:3003/todos/';
